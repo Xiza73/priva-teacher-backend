ce4abc1ef694abc660012182be366d58c930343a
Manuel Fajardo <75657501+Xiza73@users.noreply.github.com>
2023-06-30 17:18:19 -0500

    railway compatibility

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -12,6 +14,6 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(port);
 }
 bootstrap();
